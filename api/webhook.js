bcf76e96dcb9f9db2a9e57c4515894eb46b51965
megmegfire <lebe198601@gmail.com>
2025-12-03 23:50:16 +0900

    Ultra simple test

--- api/webhook.js
+++ api/webhook.js
@@ -1,12 +1,3 @@
-module.exports = async (req, res) => {
-  console.log('Webhook called');
-  console.log('Method:', req.method);
-  
-  // POSTリクエストの場合
-  if (req.method === 'POST') {
-    return res.status(200).json({ message: 'OK' });
-  }
-  
-  // GETリクエストの場合
-  return res.status(200).send('Webhook endpoint is working!');
+module.exports = (req, res) => {
+  res.status(200).send('Hello from Vercel!');
 };
