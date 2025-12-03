a3496e0e6a067c3e3cc970cca0fb01d23186bc4a
megmegfire <lebe198601@gmail.com>
2025-12-04 00:23:31 +0900

    Simplify everything

--- api/webhook.js
+++ api/webhook.js
@@ -1,22 +1,3 @@
-module.exports = async (req, res) => {
-  // CORSヘッダーを追加
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', '*');
-  
-  // OPTIONSリクエスト対応
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  
-  // POSTリクエスト処理
-  if (req.method === 'POST') {
-    console.log('POST received');
-    console.log('Headers:', req.headers);
-    console.log('Body:', req.body);
-    return res.status(200).json({ status: 'OK', method: 'POST' });
-  }
-  
-  // GETリクエスト処理
-  return res.status(200).json({ status: 'OK', method: 'GET' });
+module.exports = (req, res) => {
+  res.status(200).send('OK');
 };
